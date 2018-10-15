f8ea70c9cdd967ef4f42ebeea7d105a3eb7c7086
garcila <germanarcila@gmail.com>
2018-10-15 18:58:20 -0400

    took out the API key

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="footer">The end starts and ends here</footer>
+  <footer className="footer">
+    <p>The end starts and ends here</p>
+    <p className="footer--small" >
+      Crafted not by a 	<span role="img" aria-label="robot">&#x1F916;</span> 
+      on a <span role="img" aria-label="computer">&#128187;</span> in  
+      <span role="img" aria-label="canada">&#x1F341;</span>.
+    </p>
+  </footer>
 );
 
 export default Footer;
